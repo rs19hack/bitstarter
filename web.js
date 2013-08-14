de1a6e77af7d5bd7493f53d4d075b8141b336677
RS19.Hack <rs19hack+awsgit@gmail.com>
2013-08-14 21:03:16 +0000

    web.js modified per assgt 3

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 23');
+var fs  = require('fs');
+var buf = fs.readFileSync('index.html','utf-8'); 
+  response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
